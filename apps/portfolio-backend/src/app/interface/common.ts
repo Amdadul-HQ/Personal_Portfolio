5299896c364e8861bee53e37fb2dea824f61cbe6
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-05-19 20:18:41 +0600

    add: turbo config

--- apps/portfolio-backend/src/app/interface/common.ts
+++ apps/portfolio-backend/src/app/interface/common.ts
@@ -1,7 +1,7 @@
-import { Role } from "@prisma/client";
+import { UserRole } from "@prisma/client";
 
 export type IAuthUser = {
     id:string,
     email:string,
-    role: Role
+    role: UserRole
 } | null
